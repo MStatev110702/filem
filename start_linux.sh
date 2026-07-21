0ed606307ad84e7dfae67af142a078a4b0e7f0d9
MStatev1107 <maxim.statev@gmail.com>
2026-07-21 21:26:47 +0200

    small changes to the linux files

--- start_linux.sh
+++ start_linux.sh
@@ -3,3 +3,5 @@ systemctl --user daemon-reload
 systemctl --user enable filem.service
 systemctl --user enable --now filem.timer
 systemctl --user start filem.service
+systemctl --user status filem.service
+systemctl --user status filem.timer
